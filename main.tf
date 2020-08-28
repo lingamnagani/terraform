ac8f62717058b848d8523de94d32739c21daa84c
lingamnagani <62939813+lingamnagani@users.noreply.github.com>
2020-08-28 13:50:07 +0200

    Delete main.tf

--- main.tf
+++
@@ -1,7 +0,0 @@
-# Configure the Azure Provider
-provider "azurerm" { 
-  subscription_id = 1180ea09-eeee-45f9-8d51-e34f041f57c8
-  client_id       = f093f7c5-c106-4d36-bd10-c6f4096a3784
-  client_secret   = B0wPBqY3mgj26Z-6MP-QPi~0VV-TZ4xy3~ 
-  tenant_id       = 00864b60-6615-48a0-b1da-667795698405
-}
