89c797bca7e4ee2ab139925eb8eb8daab950fe4a
lingamnagani <62939813+lingamnagani@users.noreply.github.com>
2020-08-28 14:23:51 +0200

    commit

--- terra.tf
+++ terra.tf
@@ -1,5 +1,7 @@
 provider "azurerm" {
   version = "1.38.0"
+  subscription_id = "1180ea09-eeee-45f9-8d51-e34f041f57c8"
+  tenant_id       = "00864b60-6615-48a0-b1da-667795698405"
 }
 #create resource group
 resource "azurerm_resource_group" "rg" {
