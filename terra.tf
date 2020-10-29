af64ba612925f88bbddb1651a1233f919cf0bf12
lingamnagani <62939813+lingamnagani@users.noreply.github.com>
2020-10-29 14:23:24 +0100

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -1,12 +1,12 @@
 provider "azurerm" {
   version = "1.38.0"
-  subscription_id = var.subscriptionid
-  tenant_id       = var.tenantid
-  client_id       = var.clientid
+  subscription_id = var.subscriptionID
+  tenant_id       = var.tenantID
+  client_id       = var.clientID
   client_secret   = var.clientsecret
 }
 #create resource group
 resource "azurerm_resource_group" "rg" {
     name     = var.sampledemo321
-  location =  var.location
+    location =  var.location
 }
