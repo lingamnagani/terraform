e7368e2b1f71c012fa44b53b186e6ebd6230f263
lingamnagani <62939813+lingamnagani@users.noreply.github.com>
2020-10-29 14:08:05 +0100

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,18 @@
+variable "sampledemo321" {
+    type = string
+}
+variable "location" {
+    type = string
+}
+variable "subscriptionid " {
+    type = string
+}
+variable "clientid" {
+    type = string
+}
+variable "clientsecret" {
+    type = string
+}
+variable "tenantid" {
+    type = string
+}
