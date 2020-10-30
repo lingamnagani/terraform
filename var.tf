6664c0591529b85fedbf06123440be7ce4513a20
lingamnagani <62939813+lingamnagani@users.noreply.github.com>
2020-10-30 06:43:01 +0100

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,8 @@
+variable "testing" {
+    description = "resource grouop name"
+    default     = "testdemo"
+}
+variable "location" {
+    description = "location name"
+    default     = "canada central"
+}
